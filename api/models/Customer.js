9135f8a1ef3588ba2a1345356031ecd71f5bc4a0
arfruin <afruin@unomaha.edu>
2016-03-15 21:24:48 -0500

    Github lost its path had to copy and paste old info into new node_financial here it is!

---
+++ api/models/Customer.js
@@ -0,0 +1,54 @@
+/**
+ * Customer.js
+ *
+ * @description :: Customer contact information stored here
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  schema: true,
+
+  attributes: {
+    name: {
+      type: 'string',
+      required: true
+    },
+    street_address: {
+      type: 'string',
+    },
+    city: {
+      type: 'string'
+    },
+    state: {
+      type: 'string'
+    },
+    email: {
+      type: 'string',
+    },
+
+    zip: {
+      type: 'string'
+    },
+
+    home_phone: {
+      type: 'string'
+    },
+
+    cell_phone: {
+      type: 'string'
+    },
+
+    stocks: {
+      collection: 'stock',
+      via: 'owner'
+    },
+
+    investments: {
+      collection: 'investment',
+      via: 'owner'
+    }
+
+  }
+};
+
